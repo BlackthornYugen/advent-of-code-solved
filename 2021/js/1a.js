45d8595bdbc3d10ad71c21085c5d83068d1de3e1
John Steel <git@jskw.dev>
2021-12-02 07:36:33 -0500

    forgot to cast

--- 2021/js/1a.js
+++ 2021/js/1a.js
@@ -2,18 +2,23 @@ const aocLib = require('../../lib/aoc')
 
 function doTheSTuff(data) { // data = '1\n2\n3\n4'
     let dataArray = data.split('\n')
+    let intArray = dataArray.map((_) => parseInt(_, 10));
     let dataLength = dataArray.length // dataLength = ##
     let solution = 0
 
     for (let dataCount = 0; dataCount < dataLength -1; dataCount++ ){
-        if (dataArray[dataCount] < dataArray[dataCount+1]) {
-            solution++
+        if (intArray[dataCount] < intArray[dataCount+1]) {
+            if (dataArray[dataCount] < dataArray[dataCount+1]) {
+                solution++
+            } else {
+                debugger
+            }
         }
     }
 
     console.log(solution); // stupid internets // Stupid internets
 }
 
-
 // Run first // they are talking about part #2
 aocLib.getTestData(process.argv[3], doTheSTuff);
+
