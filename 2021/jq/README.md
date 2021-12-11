945f364edf996f397d891f4cf05ed263c4a20399
John Steel <git@jskw.dev>
2021-12-11 13:03:16 -0500

    jq 1a/1b solution + tests

---
+++ 2021/jq/README.md
@@ -0,0 +1,22 @@
+# Run with
+
+```
+shellspec --color spec/jq_spec.sh
+```
+
+# Results will look something like:
+
+```
+Running: /bin/sh [bash 3.2.57(1)-release]
+
+2021 jq advent of code 1a
+  script must output 7 for 2021/1aDemo.input
+  script must output 1692 for 2021/1aPants.input
+  script must output 1475 for 2021/1aBlackthorn.input
+2021 jq advent of code 1b
+  script must output 5 for 2021/1aDemo.input
+  script must output 1516 for 2021/1aBlackthorn.input
+
+Finished in 0.93 seconds (user 0.53 seconds, sys 0.41 seconds)
+5 examples, 0 failures
+```
--- spec/jq_spec.sh
+++ spec/jq_spec.sh
@@ -1,13 +1,31 @@
 # shellcheck disable=SC1073,SC2148
-Describe '2021 jq advent of code'
+Describe '2021 jq advent of code 1a'
   Parameters
-    1aDemo "7"
-    1aPants "1692"
+    1aDemo       "7"
+    1aPants      "1692"
     1aBlackthorn "1475"
   End
 
-  It "1a.jq should say $2 for $1"
-    When run jq --slurp -c --from-file "2021/jq/1a.jq" "2021/$1.input"
+  It "script must output $2 for 2021/$1.input"
+    When run jq --slurp --from-file "2021/jq/1.jq" "2021/$1.input"
+    The line 1 of stderr should match pattern '*DEBUG*window":[[]*[]],*hits":0*'
+    The line 2 of stderr should match pattern '*DEBUG*window":[[]*[]],*hits":*'
+    The output should eq "$2"
+  End
+End
+
+Describe '2021 jq advent of code 1b'
+  Parameters
+    1aDemo "5"
+    1aBlackthorn "1516"
+  End
+
+  It "script must output $2 for 2021/$1.input"
+    When run jq --slurp --arg window 3 --from-file "2021/jq/1.jq" "2021/$1.input"
+    The line 1 of stderr should match pattern '*DEBUG*window":[[]*[]],*hits":0*'
+    The line 2 of stderr should match pattern '*DEBUG*window":[[]*,*[]],*hits":0*'
+    The line 3 of stderr should match pattern '*DEBUG*window":[[]*,*,*[]],*hits":0*'
+    The line 4 of stderr should match pattern '*DEBUG*window":[[]*,*,*[]],*hits":*'
     The output should eq "$2"
   End
 End
