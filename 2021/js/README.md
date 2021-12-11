34547cea3e06f019b69670b7956ac4f606a16a08
John Steel <git@jskw.dev>
2021-12-11 13:03:16 -0500

    readme + tests

---
+++ 2021/js/README.md
@@ -0,0 +1,16 @@
+# Run with
+
+```
+shellspec --color spec/js_spec.sh
+```
+
+# Results will look something like: 
+
+```
+Running: /bin/sh [bash 3.2.57(1)-release]
+
+2021 js advent of code
+  script must output 7 for 1aDemo
+  script must output 1692 for 1aPants (FAILED - 1)
+  script must output 1475 for 1aBlackthorn (FAILED - 2)
+```
--- spec/js_spec.sh
+++ spec/js_spec.sh
@@ -1,13 +1,13 @@
 # shellcheck disable=SC1073,SC2148
-Describe '2021 jq advent of code'
+Describe '2021 js advent of code'
   Parameters
     1aDemo       "7"
-    1aPants      "7"
-    1aBlackthorn "7"
+    1aPants      "1692"
+    1aBlackthorn "1475"
   End
 
-  It "js should say $2 for $1"
-    When run node "2021/js/$1.js" -- "2021/$1.input"
+  It "script must output $2 for $1"
+    When run node "2021/js/1a.js" -- "2021/$1.input"
     The output should eq "$2"
   End
 End
