93b202be405f6ba55f0151ec622ca2c1fbcb6032
John Steel <john@jskw.dev>
2022-12-01 23:36:24 -0500

    1.2 pass

--- 2022/pwsh/1.ps1
+++ 2022/pwsh/1.ps1
@@ -3,20 +3,27 @@
 $content = Get-Content ../input.1
 $elf_callories = New-Object Collections.Generic.List[Int]
 $elf_callories.Add(0)
-$highest_cals = -1
-$highest_cals_index = -1
-foreach($line in $content) {
+$highest_cals = (-1, -1, -1)
+foreach ($line in $content) {
     $elf_index = $elf_callories.Count - 1
-    if($line -match ".") {
-        $elf_callories[$elf_index]+=$line
-    } else {
-        if ( $elf_callories[$elf_index] -gt $highest_cals ) {
-            $highest_cals = $elf_callories[$elf_index]
-            $highest_cals_index = $elf_index
+    if ($line -match ".") {
+        $elf_callories[$elf_index] += $line
+    }
+    else {
+        for ($i = 0; $i -lt $highest_cals.Count; $i++) {
+            if ( $elf_callories[$elf_index] -gt $highest_cals[$i] ) {
+                $highest_cals[$i] = $elf_callories[$elf_index]
+                
+                # Re-sort the list so that we are always replacing the elf with the fewest calories
+                $highest_cals = $highest_cals | Sort-Object
+                break
+            }
         }
         $elf_index++
         $elf_callories.Add(0);
     }
 }
 
-$highest_cals
+$total = 0
+$highest_cals | ForEach-Object { $total += $_ }
+$total
