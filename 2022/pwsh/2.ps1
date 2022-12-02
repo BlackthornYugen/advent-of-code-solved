ffc68e345cf4cd19210a98c382eafa4b9ca7ab39
John Steel <john@jskw.dev>
2022-12-02 13:26:18 -0500

    2.1 pass

--- 2022/pwsh/2.ps1
+++ 2022/pwsh/2.ps1
@@ -29,10 +29,11 @@ function Resolve-Game {
     param (
         [string]$game_input
     )
-    
+        
     $result = [PSCustomObject]@{ 
         Score   = 0
-        Outcome = "" 
+        Outcome = ""
+        Description = ""
     }
     
     $player_1, $player_2 = $game_input.Split(" ")
@@ -45,22 +46,27 @@ function Resolve-Game {
         throw "$player_2 is an invalid shape!"
     }
 
-    $result.Score += $shape_scores[$shapes[$player_1]]
+    $result.Score += $shape_scores[$shapes[$player_2]]
 
     if ($shapes[$player_1] -eq $shapes[$player_2]) {
+        $result.Description = "Both picked $($shapes[$player_2])."
         $result.Outcome = "Draw!"
         $result.Score += 3
-    } elseif ($shapes[$player_2] -eq $victory_conditions[$shapes[$player_1]]) {
+    } elseif ($victory_conditions[$shapes[$player_1]] -eq $shapes[$player_2]) {
+        $result.Description = "$($shapes[$player_1]) beats $($shapes[$player_2])."
+        $result.Outcome = "Defeat!"
+    } else {
+        $result.Description = "$($shapes[$player_2]) beats $($shapes[$player_1])."
         $result.Outcome = "Victory!"
         $result.Score += 6
-    } else {
-        $result.Outcome = "Defeat!"
     }
+    Write-Debug "$player_1`t$player_2`t$($result.Score)`t$($result.Outcome) $($result.Description)" 
     $result
 }
 
 function Get-Score {
     $Total = 0
+    Write-Debug "p1`tp2`tScore`tResult" 
     Get-Content $FileName | ForEach-Object{$Total += (Resolve-Game($_)).Score}
     $Total
 }
