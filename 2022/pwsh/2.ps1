33a5e3ff0a87abdc07e5d971ceab5e77a8e92bd5
John Steel <john@jskw.dev>
2022-12-02 15:25:17 -0500

    2.2 pass

--- 2022/pwsh/2.ps1
+++ 2022/pwsh/2.ps1
@@ -4,69 +4,104 @@ param (
     [string]$FileName = "../input.2"
 )
 
-$shapes = @{ 
+$Shapes = @{ 
     'A' = 'Rock'
     'B' = 'Paper'
     'C' = 'Scissors'
-    'X' = 'Rock'
-    'Y' = 'Paper'
-    'Z' = 'Scissors'
 }
 
-$shape_scores = @{ 
+$Strategies = @{
+    'X' = 'Loose'
+    'Y' = 'Draw'
+    'Z' = 'Win'
+}
+
+$Shape_Scores = @{ 
     'Rock'     = 1
     'Paper'    = 2
     'Scissors' = 3
 }
 
-$victory_conditions = @{
+$Victory_Conditions = @{
     'Rock'     = 'Scissors' # rock crushes scissors 
     'Paper'    = 'Rock'     # paper covers rock
     'Scissors' = 'Paper'    # scissors cuts paper
+}
+
+function Pick_Shape() {
+    param (
+        [Parameter(Mandatory=$true)]
+        $Opponent_Shape_Name,
+        [Parameter(Mandatory=$true)]
+        $Strategy
+    )
+
+    if ($Strategy -eq 'Win') {
+        $Player_Shape_Name = $Victory_Conditions.GetEnumerator() `
+            | Where-Object -Property Value -eq $Opponent_Shape_Name `
+            | Select-Object -First 1 `
+            | ForEach-Object Name
+    } elseif ($Strategy -eq 'Loose') {
+        $Player_Shape_Name = $Victory_Conditions[$Opponent_Shape_Name]
+    } else {
+        $Player_Shape_Name = $Opponent_Shape_Name
+    }
+
+    # Return shape code from shape name
+    return $Player_Shape_Name
+
 }
 
 function Resolve-Game {
     param (
         [string]$game_input
     )
-        
+
     $result = [PSCustomObject]@{ 
         Score   = 0
         Outcome = ""
         Description = ""
     }
     
-    $player_1, $player_2 = $game_input.Split(" ")
+    $Opponent_Shape_Code, $Strat = $game_input.Split(" ")
+    $Opponent = $Shapes[$Opponent_Shape_Code]
+    $Strategy = $Strategies[$Strat]
 
-    if (!$shapes[$player_1]) {
-        throw "$player_1 is an invalid shape!"
+    if (!$Opponent) {
+        throw "$Opponent is an invalid shape!"
     }
 
-    if (!$shapes[$player_2]) {
-        throw "$player_2 is an invalid shape!"
+    if (!$Strategy) {
+        throw "$Strat an invalid stratagy!"
     }
 
-    $result.Score += $shape_scores[$shapes[$player_2]]
+    $Player = Pick_Shape $Opponent $Strategy
 
-    if ($shapes[$player_1] -eq $shapes[$player_2]) {
-        $result.Description = "Both picked $($shapes[$player_2])."
+    if (!$Player) {
+        throw "Failed to select a move!"
+    }
+
+    $result.Score += $Shape_Scores[$Player]
+
+    if ($Player -eq $Opponent) {
+        $result.Description = "Both picked $Opponent."
         $result.Outcome = "Draw!"
         $result.Score += 3
-    } elseif ($victory_conditions[$shapes[$player_1]] -eq $shapes[$player_2]) {
-        $result.Description = "$($shapes[$player_1]) beats $($shapes[$player_2])."
+    } elseif ($Victory_Conditions[$Opponent] -eq $Player) {
+        $result.Description = "$Opponent beats $Player."
         $result.Outcome = "Defeat!"
     } else {
-        $result.Description = "$($shapes[$player_2]) beats $($shapes[$player_1])."
+        $result.Description = "$Player beats $Opponent."
         $result.Outcome = "Victory!"
         $result.Score += 6
     }
-    Write-Debug "$player_1`t$player_2`t$($result.Score)`t$($result.Outcome) $($result.Description)" 
+    Write-Debug "$Opponent`t$($Strategies[$Strat])`t$Player`t$($result.Score)`t$($result.Outcome) $($result.Description)" 
     $result
 }
 
 function Get-Score {
     $Total = 0
-    Write-Debug "p1`tp2`tScore`tResult" 
+    Write-Debug "Opponent`tStrat`tPlayer`tScore`tResult" 
     Get-Content $FileName | ForEach-Object{$Total += (Resolve-Game($_)).Score}
     $Total
 }
