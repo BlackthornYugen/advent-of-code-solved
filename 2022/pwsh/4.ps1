efd6f51bc1b2dbb77715162f97cb255ae6c28aeb
John Steel <john@jskw.dev>
2022-12-05 11:29:38 -0500

    use regex

--- 2022/pwsh/4.ps1
+++ 2022/pwsh/4.ps1
@@ -4,32 +4,40 @@ param (
     [string]$FileName = "./2022/input.4"
 )
 
-Get-Content $FileName | ForEach-Object {
-    [int]$xLow, [int]$xHigh, [int]$yLow, [int]$yHigh = $_.split(",") | ForEach-Object{ $_.split("-") }
+Get-Content $FileName 
+| ForEach-Object {
+    # Use the regular expression to match the input data
+    if ($_ -match '(\d+)-(\d+),(\d+)-(\d+)') {
+        # Extract the values from the input data
+        $xLow  = [int]$Matches[1]
+        $xHigh = [int]$Matches[2]
+        $yLow  = [int]$Matches[3]
+        $yHigh = [int]$Matches[4]
 
-    # Test if x is within y->Y
-    #  x-----X
-    # y--------Y
-    if (($xLow -ge $yLow) -and ($xLow -le $yHigh))
-    {
-        return $true
+        # Test if x is within y->Y
+        #  x-----X
+        # y--------Y
+        if (($xLow -ge $yLow) -and ($xLow -le $yHigh))
+        {
+            return $true
+        }
+
+        # Test if X is within y->Y
+        #  x-----X
+        # y--------Y
+        if (($xHigh -ge $yLow) -and ($xHigh -le $yHigh))
+        {
+            return $true
+        }
+
+        # Test if y is within x->X
+        #  y-----Y
+        # x--------X
+        if (($yLow -ge $xLow) -and ($yLow -le $xHigh))
+        {
+            return $true
+        }
+
+        # Code will never reach this point if Y is within x-----X.
     }
-
-    # Test if X is within y->Y
-    #  x-----X
-    # y--------Y
-    if (($xHigh -ge $yLow) -and ($xHigh -le $yHigh))
-    {
-        return $true
-    }
-
-    # Test if y is within x->X
-    #  y-----Y
-    # x--------X
-    if (($yLow -ge $xLow) -and ($yLow -le $xHigh))
-    {
-        return $true
-    }
-
-    # Code will never reach this point if Y is within x-----X.
 } | Measure-Object | ForEach-Object Count
