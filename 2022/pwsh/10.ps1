e3c895e5a2e654332b2002cc42aca627c311acac
John Steel <john@jskw.dev>
2022-12-10 18:56:35 -0500

    10.2 pass (330ms)

--- 2022/pwsh/10.ps1
+++ 2022/pwsh/10.ps1
@@ -4,7 +4,6 @@ param (
     [string]$FileName = "./2022/10.input"
 )
 
-
 function Step-Tube() 
 {
     [CmdletBinding()]
@@ -20,7 +19,7 @@ function Step-Tube()
     }
 
     process {
-        if ($_ -match "^(addx|noop) ?(-?\d+)?") {
+        if ($Line -match "^(addx|noop) ?(-?\d+)?") {
             [string]$instruction = $Matches[1]
             
             switch ($instruction) {
@@ -36,18 +35,55 @@ function Step-Tube()
     }
 }
 
+
+function Draw() 
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [System.Collections.ObjectModel.Collection[System.Tuple[int, int]]] $Cycles
+    )
+
+    begin {
+        $pos = [System.Console]::GetCursorPosition()
+        $displayHeight = 6
+        $displayWidth = 40
+    }
+
+    process {
+        [System.Console]::SetCursorPosition(0, $pos.Item2)
+        for ($y = 0; $y -lt $displayHeight; $y++) {
+            for ($x = 1; $x -le $displayWidth; $x++) {
+                $xVal = $Cycles | Where-Object -Property Item1 -LE ($x + $y * $displayWidth)
+                             | Measure-Object -Sum -Property Item2 
+                             | ForEach-Object Sum
+                if (($x -ge ($xVal)) -and ($x -le $xVal+2)) {
+                    Start-Sleep -Milliseconds 1
+                    Write-Host "#" -NoNewline
+                } else {
+                    Write-Host "." -NoNewline
+                }
+            }
+            Write-Host "--$($y * $displayWidth)`n" -NoNewline
+        }
+    }
+}
+
 $Cycles = Get-Content $FileName | Step-Tube 
 
 $sampleCycles = 20, 60, 100, 140, 180, 220
 
 $answer = 0
 foreach ($sampleCycle in $sampleCycles) {
-    $unprocessedSignal = ($Cycles 
+    $unprocessedSignal = $Cycles 
     | Where-Object -Property Item1 -LE $sampleCycle
     | Measure-Object -Sum -Property Item2 
-    | ForEach-Object Sum)
+    | ForEach-Object Sum
     $signal = $unprocessedSignal * $sampleCycle
     Write-Debug "$sampleCycle`t$unprocessedSignal`t$signal"
     $answer += $signal
 }
 $answer
+
+
+Draw $Cycles
