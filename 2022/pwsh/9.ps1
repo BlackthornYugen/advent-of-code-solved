4caca6d5490b13e3d18d31ed28d21ef186ed5153
John Steel <john@jskw.dev>
2022-12-10 06:20:34 -0500

    9.1 Pass (117ms)

--- 2022/pwsh/9.ps1
+++ 2022/pwsh/9.ps1
@@ -1,24 +1,43 @@
 #/usr/bin/env pwsh
 
 param (
-    [string]$FileName = "./2022/9.input"
+    [string]$FileName = "./2022/9.input",
+    [bool]$DrawThings = $false
 )
-Clear-Host
-[Console]::CursorVisible = $false
-[Console]::SetCursorPosition(0,0)
 
-$xMax = [Console]::BufferWidth
-$yMax = [Console]::BufferHeight
+if ($DrawThings)
+{
+    Clear-Host
+    [Console]::CursorVisible = $false
+    [Console]::SetCursorPosition(0,0)
+}
 
-$headPos = [System.Tuple]::Create(5, 5)
+$headPos = [System.Tuple]::Create(0, 0)
 $tailPos = $headPos
-[Console]::SetCursorPosition($currentPosition.Item1, $currentPosition.Item2)
 
 $Directions = @{
-    "U" = [System.Tuple]::Create(  0, -1 )
-    "R" = [System.Tuple]::Create(  1,  0 )
-    "D" = [System.Tuple]::Create(  0,  1 )
-    "L" = [System.Tuple]::Create( -1,  0 )
+    U = [System.Tuple]::Create(  0, -1 )
+    R = [System.Tuple]::Create(  1,  0 )
+    D = [System.Tuple]::Create(  0,  1 )
+    L = [System.Tuple]::Create( -1,  0 )
+}
+
+$TailMoves = @{
+    [System.Tuple]::Create(  0, -2 ) = $Directions.U
+    [System.Tuple]::Create(  1, -2 ) = $Directions.U
+    [System.Tuple]::Create( -1, -2 ) = $Directions.U
+
+    [System.Tuple]::Create(  2,  0 ) = $Directions.R
+    [System.Tuple]::Create(  2,  1 ) = $Directions.R
+    [System.Tuple]::Create(  2, -1 ) = $Directions.R
+
+    [System.Tuple]::Create(  0,  2 ) = $Directions.D
+    [System.Tuple]::Create(  1,  2 ) = $Directions.D
+    [System.Tuple]::Create( -1,  2 ) = $Directions.D
+
+    [System.Tuple]::Create( -2,  0 ) = $Directions.L
+    [System.Tuple]::Create( -2,  1 ) = $Directions.L
+    [System.Tuple]::Create( -2, -1 ) = $Directions.L
 }
 
 $TailHistory = @{}
@@ -32,7 +51,7 @@ function draw() {
 
     [Console]::SetCursorPosition($x * 4, $y * 2)
     Write-Host $value -NoNewline
-    
+    Start-Sleep -Milliseconds 200
 }
 # Write-Host "S" -NoNewline
 
@@ -45,22 +64,27 @@ Get-Content $FileName | ForEach-Object {
 
         for ($i = 0; $i -lt $distance; $i++) {
 
-            draw $tailPos.Item1 $tailPos.Item2 "#"
-            draw $headPos.Item1 $headPos.Item2 " "
+            # draw $tailPos.Item1 $tailPos.Item2 "#"
+            # draw $headPos.Item1 $headPos.Item2 " "
 
             $headPos = [System.Tuple]::Create(
                 $headPos.Item1 + $direction.Item1,
                 $headPos.Item2 + $direction.Item2
             )
 
-            $diffX = (($headPos.Item1 - $tailPos.Item1) / 2)
-            $diffY = (($headPos.Item2 - $tailPos.Item2) / 2)
-            $xxx = if ($direction.Item2 -eq 0) { [int]$diffX + $tailPos.Item1 } else { $headPos.Item1 }
-            $yyy = if ($direction.Item1 -eq 0) { [int]$diffY + $tailPos.Item2 } else { $headPos.Item2 }
-            # if (!(([Math]::Abs($diffX) -eq 1) -or ([Math]::Abs($diffY) -eq 1)))
-            if (!(([Math]::Abs($diffX) -eq 0.5) -and ([Math]::Abs($diffY) -eq 0.5)))
+            $deltaPos = [System.Tuple]::Create(
+                $tailPos.Item1 - $headPos.Item1,
+                $tailPos.Item2 - $headPos.Item2
+            )
+
+            $newTailRelativePos = $TailMoves[$deltaPos]
+
+            if ($null -ne $newTailRelativePos)
             {
-                $tailPos = [System.Tuple]::Create($xxx, $yyy)
+                $tailPos = [System.Tuple]::Create(
+                    $newTailRelativePos.Item1 + $headPos.Item1,
+                    $newTailRelativePos.Item2 + $headPos.Item2
+                )
             }
             
             if ($TailHistory.Contains($tailPos)) {
@@ -68,11 +92,12 @@ Get-Content $FileName | ForEach-Object {
             } else {
                 $TailHistory.Add($tailPos, 1)
             }
-            draw $headPos.Item1 $headPos.Item2 "H"
-            draw $tailPos.Item1 $tailPos.Item2 "T$($TailHistory[$tailPos])"
 
-            [Console]::SetCursorPosition($xMax, $yMax)
-            Start-Sleep -Milliseconds 200
+            if ($DrawThings)
+            {
+                draw $headPos.Item1 $headPos.Item2 "H"
+                draw $tailPos.Item1 $tailPos.Item2 "T$($TailHistory[$tailPos])"
+            }
         }
 
     }
@@ -83,11 +108,12 @@ Get-Content $FileName | ForEach-Object {
 
 }
 
-# Start-Sleep 3
-[Console]::SetCursorPosition(0,0)
-Write-Host "                "
-Write-Host "                "
-Write-Host "                "
-[Console]::SetCursorPosition(0,0)
-Write-Host "                "
+if ($DrawThings) {
+    [Console]::SetCursorPosition(0,0)
+    Write-Host "                "
+    Write-Host "                "
+    Write-Host "                "
+    [Console]::SetCursorPosition(0,0)
+    Write-Host "                "
+}
 Write-Host $TailHistory.Count
