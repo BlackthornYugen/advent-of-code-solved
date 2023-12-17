c18c3afdff47bb7fecb869672f426e06cd88fbf1
John Steel <john@jskw.dev>
2023-12-16 21:59:45 -0500

    time runs

--- 2023/jq/run-modified.sh
+++ 2023/jq/run-modified.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -x
+suffix=${suffix:-a.sample}
 while sleep 2 ; do 
     TRIGGER=$(fswatch --one-event *.jq)
-    $TRIGGER "${TRIGGER%%.jq}a.sample"
+    time $TRIGGER "${TRIGGER%%.jq}${suffix}"
 done
