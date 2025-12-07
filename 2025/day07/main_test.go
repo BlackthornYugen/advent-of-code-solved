94b2f7466e3970a05e3f890fd97d8d26cab0c8ed
John Steel <john@jskw.dev>
2025-12-07 20:37:49 +0000

    2025 go 7.2 did not finish

--- 2025/day07/main_test.go
+++ 2025/day07/main_test.go
@@ -37,8 +37,8 @@ func TestPart2(t *testing.T) {
 		file     string
 		expected string
 	}{
-		{"sample.txt", "?"},
-		{"input.txt", "?"},
+		{"sample.txt", "40"},
+		{"input.txt", "42"},
 	}
 
 	for _, tt := range tests {
