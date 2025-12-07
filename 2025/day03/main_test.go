d20311fd82f8a6f09a7b3e5398780cc48815160a
John Steel <john@jskw.dev>
2025-12-07 00:03:38 +0000

    2025 go 3.1 pass

--- 2025/day03/main_test.go
+++ 2025/day03/main_test.go
@@ -11,8 +11,8 @@ func TestPart1(t *testing.T) {
 		file     string
 		expected string
 	}{
-		{"sample.txt", "?"},
-		{"input.txt", "?"},
+		{"sample.txt", "357"},
+		{"input.txt", "16927"},
 	}
 
 	for _, tt := range tests {
