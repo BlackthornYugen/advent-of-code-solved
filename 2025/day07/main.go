f09c72c58d54baa14ad65326313abbd5e1aba16b
John Steel <john@jskw.dev>
2025-12-07 14:35:52 +0000

    Remove partially done drawing logic

--- 2025/day07/main.go
+++ 2025/day07/main.go
@@ -6,7 +6,6 @@ import (
 	"log/slog"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/BlackthornYugen/advent-of-code-solved/2025/lib"
 )
@@ -28,43 +27,34 @@ func Part1(input string) string {
 	tachyonBeamCount := 0
 	input = strings.TrimSpace(input)
 	lines := strings.Split(input, "\n")
-	// Loop while no tachyon beam '|' found on last line
-	for !strings.Contains(lines[len(lines)-1], "|") {
-		lineChanged := false
-		for activeLineNum, line := range lines {
-			fmt.Println(line)
-			for activeColumnNum, character := range line {
-				if (activeLineNum < len(lines)-1) && (string(character) == "S" || string(character) == "|") {
-					slog.Log(context.Background(), lib.TraceLogLevel, "Found beam source or beam",
-						"line", activeLineNum,
-						"column", activeColumnNum,
-						"char", string(character),
-						"lineChanged", lineChanged,
-						"tachyonBeamCount", tachyonBeamCount,
-					)
-					// if char on next line at same column is '.', replace it with '|', if '^', replace adjacent '.'s with '|'
-					switch lines[activeLineNum+1][activeColumnNum] {
-					case '.':
-						lineChanged = true
-						slog.Log(context.Background(), lib.TraceLogLevel, "Replacing . with | below S")
-						lines[activeLineNum+1] = lines[activeLineNum+1][:activeColumnNum] + "|" + lines[activeLineNum+1][activeColumnNum+1:]
-					case '^':
-						lineChanged = true
-						tachyonBeamCount++
-						slog.Log(context.Background(), lib.TraceLogLevel, "Replacing .s with |s adjacent to ^ below S")
-						// Replace left adjacent '.'
-						if activeColumnNum > 0 && lines[activeLineNum+1][activeColumnNum-1] == '.' {
-							lines[activeLineNum+1] = lines[activeLineNum+1][:activeColumnNum-1] + "|" + lines[activeLineNum+1][activeColumnNum:]
-						}
-						// Replace right adjacent '.'
-						if activeColumnNum < len(lines[activeLineNum+1])-1 && lines[activeLineNum+1][activeColumnNum+1] == '.' {
-							lines[activeLineNum+1] = lines[activeLineNum+1][:activeColumnNum+1] + "|" + lines[activeLineNum+1][activeColumnNum+2:]
-						}
+	for activeLineNum, line := range lines {
+		for activeColumnNum, character := range line {
+			if (activeLineNum < len(lines)-1) && (string(character) == "S" || string(character) == "|") {
+				slog.Log(context.Background(), lib.TraceLogLevel, "Found beam source or beam",
+					"line", activeLineNum,
+					"column", activeColumnNum,
+					"char", string(character),
+					"tachyonBeamCount", tachyonBeamCount,
+				)
+				// if char on next line at same column is '.', replace it with '|', if '^', replace adjacent '.'s with '|'
+				switch lines[activeLineNum+1][activeColumnNum] {
+				case '.':
+					slog.Log(context.Background(), lib.TraceLogLevel, "Replacing . with | below S")
+					lines[activeLineNum+1] = lines[activeLineNum+1][:activeColumnNum] + "|" + lines[activeLineNum+1][activeColumnNum+1:]
+				case '^':
+					tachyonBeamCount++
+					slog.Log(context.Background(), lib.TraceLogLevel, "Replacing .s with |s adjacent to ^ below S")
+					// Replace left adjacent '.'
+					if activeColumnNum > 0 && lines[activeLineNum+1][activeColumnNum-1] == '.' {
+						lines[activeLineNum+1] = lines[activeLineNum+1][:activeColumnNum-1] + "|" + lines[activeLineNum+1][activeColumnNum:]
+					}
+					// Replace right adjacent '.'
+					if activeColumnNum < len(lines[activeLineNum+1])-1 && lines[activeLineNum+1][activeColumnNum+1] == '.' {
+						lines[activeLineNum+1] = lines[activeLineNum+1][:activeColumnNum+1] + "|" + lines[activeLineNum+1][activeColumnNum+2:]
 					}
 				}
 			}
 		}
-		time.Sleep(200 * time.Millisecond)
 	}
 	return fmt.Sprintf("%d", tachyonBeamCount)
 }
